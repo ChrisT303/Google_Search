b1d3b4c0a4b3cb16908ec56959c175acd8821b54
deftonechris <deftonechris@msn.com>
2022-10-04 18:41:15 -0600

    added mongo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Books', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://chris123:Wavingcat1@cluster0.cegxu0b.mongodb.net/book_search?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 module.exports = mongoose.connection;
