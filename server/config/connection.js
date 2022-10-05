f5e7f2550b44d1782cb5cd9f0b441f03580ac78f
deftonechris <deftonechris@msn.com>
2022-10-05 14:25:15 -0600

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,15 +3,26 @@ require('dotenv').config();
 
 // added new database
 mongoose.connect(
-  process.env.MONGODB_URI ||
-    "mongodb://localhost:27017/book_search",
+  process.env.MONGODB_URI || "mongodb://localhost/book_search",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  },
+    useCreateIndex: true,
+    useFindAndModify: true,
+  }
 );
 
 module.exports = mongoose.connection;
 
 
 // mongodb+srv://deftonechris:zarblat@cluster0.f3y28om.mongodb.net/book_search?retryWrites=true&w=majority
+
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://localhost:27017/book_search",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: true,
+//   }
+// );
