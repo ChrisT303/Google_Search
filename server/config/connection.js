f0648bf2fd697090e3f2d67c5228830a1ba24e5b
deftonechris <deftonechris@msn.com>
2022-10-04 19:18:09 -0600

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+// added new database
 mongoose.connect(
   process.env.MONGODB_URI ||
     "mongodb://localhost:27017/book_search",
