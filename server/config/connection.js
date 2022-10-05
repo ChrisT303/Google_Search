11c44eb90d69a19d3d12480cebbd25a0edfd4a5a
deftonechris <deftonechris@msn.com>
2022-10-04 18:53:14 -0600

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-    "mongodb+srv://chris123:deftonecchris@cluster0.cegxu0b.mongodb.net/book_search?retryWrites=true&w=majority",
+    "mongodb://localhost:27017/book_search",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
