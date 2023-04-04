5d1b0e6e481671c2bc2776635ae5736551512053
deftonechris <deftonechris@msn.com>
2023-04-03 18:09:13 -0600

    Changed connection config

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-// added new database
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/book_search",
+  process.env.MONGODB_URI,
+
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
+console.log("MONGODB_URI:", process.env.MONGODB_URI);
 
 module.exports = mongoose.connection;
-
-// mongodb+srv://deftonechris:zarblat@cluster0.f3y28om.mongodb.net/book_search?retryWrites=true&w=majority
